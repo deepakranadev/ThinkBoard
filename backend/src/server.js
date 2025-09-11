775b734402a7a72de193acb1d01cfbbbb0f09b39
deepakranadev <connectwithdeepakrana@gmail.com>
2025-09-12 00:54:43 +0530

    prepared for the developement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 
 import notesRouter from './routes/notesRoutes.js'
 import { connect } from './config/db.js';
@@ -8,19 +9,31 @@ import rateLimiter from './middlewares/ratelimiter.js';
 
 const app = express();
 const PORT = process.env.PORT || 5001;  
+const __dirname = path.resolve()
 
 dotenv.config()
 
 
-app.use(cors({
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors({
     origin:"http://localhost:5173",
 })
 );
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
 
 app.use('/api/notes',notesRouter)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 connect().then(()=>{
     app.listen(PORT,()=>{
